4f5e11ddc579f16c9961ba034ac2c7888a026bf7
Peter Lam <plam1216@gmail.com>
2023-02-20 12:46:37 -0500

    get videos from GraphCMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
+// Documentation
+// https://www.npmjs.com/package/graphql-request
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, GraphQLClient } from 'graphql-request'
+
 
 function App() {
+  const getData = async () => {
+    const url: string = process.env.REACT_APP_GRAPH_CMS_ENDPOINT!
+    const token: string = process.env.REACT_APP_GRAPH_CMS_TOKEN!
+
+    const client = new GraphQLClient(url, {
+      headers: {
+        "Authorization": "Bearer " + token
+      }
+    })
+
+    const query = gql`
+      query {
+        videos {
+          id,
+          title,
+          createdAt,
+        }
+      }
+    `
+    const data = await client.request(query)
+    const { videos } = data
+
+    console.log(videos)
+
+    return
+  }
+
+  getData()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
